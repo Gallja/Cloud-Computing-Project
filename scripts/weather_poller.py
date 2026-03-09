c159b77453f67c01dd99ab10fef163c9ac1aba6a
Gallja <andrea.galliano2@studenti.unimi.it>
2026-03-09 15:20:00 +0100

    update poller

--- scripts/weather_poller.py
+++ scripts/weather_poller.py
@@ -4,45 +4,62 @@ import time
 API_PRODUCER_URL = "http://127.0.0.1:8000/weather"
 
 CITIES = {
-    "Milano": {"lat": 45.46427, "lon": 9.18951},
-    "Torino": {"lat": 45.07049, "lon": 7.68682},
+    "Milan": {"lat": 45.46427, "lon": 9.18951},
+    "Turin": {"lat": 45.07049, "lon": 7.68682},
     "Verona": {"lat": 45.4299, "lon": 10.9844},
-    "Firenze": {"lat": 43.77925, "lon": 11.24626},
-    "Roma": {"lat": 41.89193, "lon": 12.51133},
-    "Napoli": {"lat": 40.85631, "lon": 14.24641}
+    "Florence": {"lat": 43.77925, "lon": 11.24626},
+    "Rome": {"lat": 41.89193, "lon": 12.51133},
+    "Naples": {"lat": 40.85631, "lon": 14.24641}
 }
 
 def fetch_and_send_weather():
-    print("Raccolta dati meteo...")
+    print("Weather data polling...")
     
+    current_vars = "temperature_2m,wind_speed_10m,is_day,weather_code,precipitation,rain,cloud_cover,surface_pressure"
+
     while True:
         for city_name, coords in CITIES.items():
-            meteo_url = f"https://api.open-meteo.com/v1/forecast?latitude={coords['lat']}&longitude={coords['lon']}&current_weather=true"
+            meteo_url = (f"https://api.open-meteo.com/v1/forecast?"
+                         f"latitude={coords['lat']}&longitude={coords['lon']}"
+                         f"&current={current_vars}"
+                         f"&daily=sunshine_duration"
+                         f"&timezone=auto")
             
             try:
                 response = requests.get(meteo_url, timeout=5)
                 if response.status_code == 200:
                     data = response.json()
+
                     current = data.get("current_weather", {})
+                    daily = data.get("daily", {})
+
+                    sunshine_today = 0
+                    if "sunshine_duration" in daily and len(daily["sunshine_duration"]) > 0:
+                        sunshine_today = daily["sunshine_duration"][0]
                     
                     payload = {
                         "city": city_name,
-                        "temperature": current.get("temperature"),
-                        "windspeed": current.get("windspeed"),
+                        "temperature": current.get("temperature_2m"),
+                        "windspeed": current.get("wind_speed_10m"),
                         "is_day": current.get("is_day"),
-                        "weathercode": current.get("weathercode")
+                        "weathercode": current.get("weather_code"),
+                        "precipitation": current.get("precipitation"),
+                        "rain": current.get("rain"),
+                        "cloud_cover": current.get("cloud_cover"),
+                        "surface_pressure": current.get("surface_pressure"),
+                        "sunshine_duration": sunshine_today
                     }
                     
                     post_response = requests.post(API_PRODUCER_URL, json=payload, timeout=4)
-                    print(f"Inviato dato per {city_name} - Status API: {post_response.status_code}")
-                    # print(f"Dato estratto per {city_name}: {payload}")
+                    print(f"Data sended for {city_name} - Status API: {post_response.status_code}")
+                    # print(f"Data extracted for {city_name}: {payload}")
                     
             except Exception as e:
-                print(f"Errore durante l'elaborazione di {city_name}: {e}")
+                print(f"Error during elaboration - {city_name}: {e}")
             
             time.sleep(2)
             
-        print("Ciclo completato.")
+        print("Cycle completed.")
         time.sleep(2)
 
 if __name__ == "__main__":
